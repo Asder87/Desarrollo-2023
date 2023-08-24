02da3789f651c8ae91985d9f4196baf3bf7b03aa
Asder87 <asder8000@gmail.com>
2023-08-24 11:49:32 -0600

    Va

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 - p01-hola-mundo
 - p02-paga-trabajador
 - p03-venta-pizzas
+
+## Actividad 2
+
+- p04-procesa-calificaciones
+- p05-paga-trabajo
+- p06-factorial-numeros
---
+++ p05-paga-trabajo/Program.cs
@@ -0,0 +1,20 @@
+﻿// Imprime una tabla de conversion de peso a otras monedas
+int ini, fin, c;
+float tcd=17.70f, tcl=21.21f, tce=18.19f, tca=0.048f;
+
+Console.Clear();
+Console.WriteLine("Imprime tabla de conversion de monedas\n");
+
+do{
+    Console.WriteLine("Inicio: "); ini = int.Parse(Console.ReadLine());
+    Console.WriteLine("Fin  : "); fin = int.Parse(Console.ReadLine());
+} while(fin<ini);
+
+c=ini;
+Console.WriteLine("Peso\tDolar\tLibra\tEuro\tMessi Peso");
+Console.WriteLine(new string('-',43));
+while(c<=fin){
+    Console.WriteLine($"{c:f2}\t{c/tcd:f2}\t{c/tcl:f2}\t{c/tce:f2}\t{c/tca:f2}");
+    c++;
+}
+Console.WriteLine(new string('-',43));
